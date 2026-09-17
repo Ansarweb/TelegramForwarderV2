7d59cda1ce8bb6b14ade87f233aefd995d530f01
Ansarweb <87944271+Ansarweb@users.noreply.github.com>
2026-09-17 19:08:42 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,11 +8,14 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "TelegramForwarderV2"
+
 include(":app")
